8cac1916a270827ccd3ebee5bb028b295e757ba6
Kate Kopteva <bychkokatya@gmail.com>
2021-07-26 14:49:17 +0300

    Минимальный элемент матрицы

--- task_6_1.py
+++ task_6_1.py
@@ -1,6 +1,7 @@
 from random import randint
 n, m, a, b = int(input()), int(input()), int(input()), int(input())
 maxi = a
+mini = b
 matr = []
 for i in range(n):
     l = []
@@ -9,6 +10,9 @@ for i in range(n):
         l.append(r)
         if r > maxi:
             maxi = r
+        if r < mini:
+            mini = r
     matr.append(l)
 print(matr)
 print(maxi)
+print(mini)
